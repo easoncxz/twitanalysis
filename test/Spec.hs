ce93c284df4fd80004978891eed32061e50c69ef
Eason Chen <easoncxz@gmail.com>
2020-08-17 00:09:24 +1200

    Write a sample de-serialisation test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy.Char8 as BSL8
+import Test.Hspec (describe, hspec, it, shouldBe)
+import qualified Web.Twitter.Types as Twitter
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $ do
+    describe "Twitter types" $ do
+      it "can be de-serialised" $
+        -- Sample taken from:
+        --    https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
+        -- Fixes to sample needed: JSON-quoting of double-quotes in the `source` fields.
+       do
+        sampleJsonBs <- BSL8.readFile "test/resources/sample-user.json"
+        case Aeson.eitherDecode sampleJsonBs of
+          Left msg -> fail msg
+          Right (status :: Twitter.Status) -> do
+            BSL8.putStrLn (Aeson.encode status)
+            Twitter.statusId status `shouldBe` 850007368138018817
