d7d192932b2d0257ef581de24af0df40fc7c7600
Eason Chen <easoncxz@gmail.com>
2020-08-19 01:58:42 +1200

    Nest static files to be served under `/static/`
    This is ridiculously complicated! Tools used:
    
    - For string prefix-matching: Text.ParserCombinators.ReadP
    - For meddling with Request values: Network.Wai.Middleware.Rewrite
    
    Plus a bunch of string-manipulation via Data.Text and some manual
    list-mangling via map, filter, and zip.
    
    But it works now! Visit:
    
    - http://localhost:5000/index.html
    
    As such, issue #1 is well-addressed now.

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,17 @@
 
 module Main where
 
+import qualified Data.ByteString.Char8 as BS8
 import Data.Monoid ((<>))
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Tuple (uncurry)
+import qualified Debug.Trace as Trace
+import qualified Network.Wai as Wai
 import Network.Wai.Middleware.RequestLogger (logStdout)
+import qualified Network.Wai.Middleware.Rewrite as Rewrite
 import qualified Network.Wai.Middleware.Static as Static
+import qualified Text.ParserCombinators.ReadP as ReadP
 import qualified Web.Scotty as Scotty
 
 import qualified Lib
@@ -15,6 +23,35 @@ scottyApp appEnv = do
   Scotty.get "/login" (Lib.startOAuthFlow appEnv)
   Scotty.get Lib.oauthCallbackPath (Lib.handleOAuthCallback appEnv)
 
+myStaticMiddleware :: Wai.Middleware
+myStaticMiddleware =
+  Static.staticPolicy
+    (trace <> Static.noDots <> Static.addBase "frontend-app/static/")
+  where
+    trace =
+      Static.predicate
+        (\s -> Trace.trace ("The static path is: " ++ show s) True)
+
+eatPrefix :: [Text] -> Wai.Request -> Wai.Request
+eatPrefix prefix =
+  Rewrite.rewriteRequestPure $ \(pathComps, query) _headers ->
+    let restComps = map snd . filter (uncurry (==)) $ zip prefix pathComps
+     in (restComps, query)
+
+parsePrefix :: String -> [Text]
+parsePrefix prefix = filter (/= "") (Text.splitOn "/" (Text.pack prefix))
+
+prefixedStatic :: String -> Wai.Middleware
+prefixedStatic prefix next request respond = do
+  putStrLn ("The rawPathInfo is: " ++ BS8.unpack (Wai.rawPathInfo request))
+  case ReadP.readP_to_S
+         (ReadP.string prefix)
+         (BS8.unpack (Wai.rawPathInfo request)) of
+    [(_thePrefix, rest)] -> do
+      putStrLn ("The rest of the rawPathInfo is: " ++ rest)
+      myStaticMiddleware next (eatPrefix (parsePrefix rest) request) respond
+    _ -> next request respond
+
 main :: IO ()
 main = do
   appEnv <- Lib.newAppEnv
@@ -22,7 +59,5 @@ main = do
     ("Using client credentials: " ++ show (Lib.appEnvOAuthClientCred appEnv))
   Scotty.scotty 5000 $ do
     Scotty.middleware logStdout
-    Scotty.middleware
-      (Static.staticPolicy
-         (Static.noDots <> Static.addBase "frontend-app/static/"))
+    Scotty.middleware (prefixedStatic "/static")
     scottyApp appEnv
