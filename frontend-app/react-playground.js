35095f4ada8d37a25ff8b5b2fcb4f642d8701357
Eason Chen <easoncxz@gmail.com>
2020-08-28 01:39:21 +1200

    Implement a start/stop toggle
    I thought I needed redux-thunk, but it turns out by rolling my own
    intuition, everything pretty much just worked.

--- frontend-app/react-playground.js
+++ frontend-app/react-playground.js
@@ -1,16 +1,18 @@
 
+/* global React, ReactDOM, Redux, ReduxThunk */
+
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 //import Redux from 'redux';
 //import ReduxThunk from 'redux-thunk';
-const thunk = ReduxThunk.default;
+//const thunk = ReduxThunk.default;
 
 const init = {
   cancelHandle: undefined,
   now: new Date(),
 };
 
-const update = (current, action) => {
+const reducer = (current, action) => {
   switch (action.type) {
   case 'tick':
     return {
@@ -22,18 +24,23 @@ const update = (current, action) => {
       ...current,
       cancelHandle: action.cancelHandle,
     };
+  case 'stop_clock':
+    return {
+      ...current,
+      cancelHandle: action.cancelHandle,
+    };
   default:
     return current;
   }
 };
 
 const store = Redux.createStore(
-  update,
+  reducer,
   init,
-  Redux.applyMiddleware(thunk),
+  //Redux.applyMiddleware(thunk),
 );
 
-const effectsOf = dispatch => ({
+const actionsOf = dispatch => ({
   tick(t) {
     return {
       type: 'tick',
@@ -49,7 +56,14 @@ const effectsOf = dispatch => ({
       ),
     };
   },
+  stopClock(handle) {
+    return {
+      type: 'stop_clock',
+      cancelHandle: void clearInterval(handle),
+    };
+  },
 });
+
 
 class Clock extends React.Component {
   constructor(props) {
@@ -57,41 +71,55 @@ class Clock extends React.Component {
     this.props = props;
   }
 
+  renderClockDescription() {
+    const props = this.props;
+    if (props.model.cancelHandle === undefined) {
+      return (
+        <div>
+          <p>(The clock is stopped.)</p>
+          <button onClick={
+            () => props.dispatch(props.actions.startClock())
+          }>Start</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>(The clock is ticking.)</p>
+          <button onClick={
+            () => props.dispatch(props.actions.stopClock(props.model.cancelHandle))
+          }>Stop</button>
+        </div>
+      );
+    }
+  }
+
   render() {
+    const styling =
+      this.props.model.cancelHandle === undefined
+        ? { class: 'red-frame' }
+        : { class: 'green-frame' };
     return (
       <div>
         <h1>The time is currently:</h1>
-        <p><code>{ this.props.model.now.toISOString() }</code></p>
-        <p>
-          (The clock is{
-            this.props.model.cancelHandle === undefined
-              ? ' not'
-              : ''
-          } ticking.)
-        </p>
+        <p><code {...styling}>{ this.props.model.now.toISOString() }</code></p>
+        {this.renderClockDescription()}
       </div>
     );
   }
 }
 
 const mountPoint = document.getElementById('root');
-
 const renderRoot = () => {
   ReactDOM.render(
     <Clock
       model={store.getState()}
+      dispatch={store.dispatch.bind(store)}
+      actions={actionsOf(store.dispatch.bind(store))}
     />,
     mountPoint
   );
 };
-
 store.subscribe(renderRoot);
 
-const effects = effectsOf(store.dispatch.bind(store));
-
-store.dispatch(effects.startClock());
-
-//setInterval(
-//  () => { store.dispatch(effects.tick(new Date())); },
-//  1000,
-//);
+renderRoot();
