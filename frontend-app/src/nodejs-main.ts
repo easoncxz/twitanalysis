54e70e5fb4e5dbeabaff151a8eca918ae0f220ed
Eason Chen <easoncxz@gmail.com>
2020-08-31 23:35:47 +1200

    Craft exe to send tweet via nodejs CLI
    Modify the source code inside `src/noejs-main.ts`, then either run:
    
        $ yarn tsc
        $ node ts-out/nodejs-main.js
    
    Or do it in one go using ts-node:
    
        $ yarn ts-node src/nodejs-main.ts
    
    It appears that POST requests with a request body of `application/json`
    and `application/x-www-form-urlencoded` are both accepted by the Twitter
    v1.1 API.
    
        https://twitter.com/easoncxz/status/1300391856316903424

--- frontend-app/src/nodejs-main.ts
+++ frontend-app/src/nodejs-main.ts
@@ -1,15 +1,13 @@
 
-//import { dirname } from 'path';
-//import { fileURLToPath } from 'url';
-//export const srcPath = dirname(fileURLToPath(import.meta.url));
-//export const isMain = () => process.argv[1] === fileURLToPath(import.meta.url);
-
 export const srcPath = __dirname;
 export const isMain = (): boolean => require.main === module;
 
-import * as OAuthClient from './oauth-client';
+import util from 'util';
 
-if (isMain()) {
+import * as OAuthClient from './oauth-client';
+import * as OtherOAuthClient from './oauth-client-again';
+
+function tryOAuthClient() {
   const args = OAuthClient.prep(
     {
       url: 'https://api.twitter.com/1.1/statuses/update.json',
@@ -28,4 +26,28 @@ if (isMain()) {
   //  .then(j => {
   //    console.log(j);
   //  });
+}
+void tryOAuthClient;
+
+function tryOtherOAuthClient() {
+  const o = OtherOAuthClient.mkDefaultOAuth();
+  //OtherOAuthClient.sendTweetWwwFormUrlencoded(
+  OtherOAuthClient.sendTweetApplicationJson(
+    o,
+    "What about application/json? Does Twitter accept this Content-Type in POST request bodies?", // eslint-disable-line quotes
+  ).then(
+    resp => {
+      console.log('Tweet result received');
+      console.log(resp);
+      console.log(util.inspect(JSON.parse(resp)));
+    },
+    err => {
+      console.log('Request errored');
+      console.log(err);
+    }
+  );
+}
+
+if (isMain()) {
+  tryOtherOAuthClient();
 }
