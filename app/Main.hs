2fa286e10db0659577a4075bcdc2684ce1dcfc65
Eason Chen <easoncxz@gmail.com>
2020-09-05 00:54:28 +1200

    Write simple session demo (worked first time!)

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,28 @@ module Main where
 
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import qualified Web.Scotty as Scotty
+import qualified Web.Scotty.Session as Session
 
+import qualified ButlerDemo as Butler
 import qualified Lib
 import qualified MiscWaiMiddleware as Middle
 import qualified TwitterOAuthConsumer as TwitterO
 
-scottyApp :: TwitterO.AppEnv -> Scotty.ScottyM ()
-scottyApp appEnv = do
+scottyApp ::
+     TwitterO.AppEnv
+  -> Session.ScottySM Butler.MySessionType
+  -> Scotty.ScottyM ()
+scottyApp appEnv sessionMan = do
   Scotty.get "/login" (Lib.startOAuthFlow appEnv)
   Scotty.get TwitterO.oauthCallbackPath (Lib.handleOAuthCallback appEnv)
+  Scotty.get Butler.butlerGetPath (Butler.recogniseGuest sessionMan)
+  Scotty.post Butler.butlerPostPath (Butler.registerGuest sessionMan)
+  Scotty.post Butler.butlerPostPathLogout (Butler.forgetGuest sessionMan)
 
 main :: IO ()
 main = do
   appEnv <- TwitterO.newAppEnv
+  sessionMan <- Session.createSessionManager
   putStrLn
     ("Using client credentials: " ++
      show (TwitterO.appEnvOAuthClientCred appEnv))
@@ -24,4 +33,4 @@ main = do
     Scotty.middleware logStdout
     Scotty.middleware Middle.justFavicon
     Scotty.middleware Middle.myStaticMiddleware
-    scottyApp appEnv
+    scottyApp appEnv sessionMan
---
+++ src/ButlerDemo.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module ButlerDemo where
+
+import Prelude hiding (div, head, id)
+
+import Data.String (IsString)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Text.Blaze.Html.Renderer.Text as BlazeT
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5 (Markup, (!))
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Web.Scotty as Scotty
+import qualified Web.Scotty.Session as Session
+
+newtype MySessionType =
+  MySessionType Text
+
+nameFieldName :: IsString s => s
+nameFieldName = "name-field"
+
+recogniseGuestView :: Maybe MySessionType -> Markup
+recogniseGuestView who =
+  H.html $ do
+    H.body $ do
+      H.h1 "Welcome, my guest!"
+      case who of
+        Nothing -> do
+          H.p "It must be your first time here!"
+          H.p "please tell us something about yourself."
+          H.form ! A.method "POST" ! A.action butlerPostPath $ do
+            H.label ! A.for nameFieldName $ "Your name:"
+            H.input ! A.name nameFieldName ! A.type_ "text"
+            H.input ! A.type_ "submit" ! A.value "There you go"
+        Just (MySessionType someone) -> do
+          H.p . H.text $
+            "Ah, you must be " <> someone <> "! I've met you last time."
+          H.p "Welcome back!"
+          H.form ! A.method "POST" ! A.action "/butler/goodbye" $ do
+            H.button $ H.text "Please forget about me"
+
+butlerGetPath :: IsString s => s
+butlerGetPath = "/butler"
+
+butlerPostPath :: IsString s => s
+butlerPostPath = "/butler/register"
+
+butlerPostPathLogout :: IsString s => s
+butlerPostPathLogout = "/butler/goodbye"
+
+recogniseGuest :: Session.ScottySM MySessionType -> Scotty.ActionM ()
+recogniseGuest sessionMan = do
+  session <- Session.readSession sessionMan
+  Scotty.html . BlazeT.renderHtml $ recogniseGuestView session
+
+registerGuest :: Session.ScottySM MySessionType -> Scotty.ActionM ()
+registerGuest sessionMan = do
+  name :: Text <- Scotty.param nameFieldName
+  Session.modifySession sessionMan (const (Just (MySessionType name)))
+  Scotty.redirect butlerGetPath
+
+forgetGuest :: Session.ScottySM MySessionType -> Scotty.ActionM ()
+forgetGuest sessionMan = do
+  Session.modifySession sessionMan (const Nothing)
+  Scotty.redirect butlerGetPath
