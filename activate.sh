d5e540b60deeb9105430e80e54216e20582458b8
Eason Chen <easoncxz@gmail.com>
2020-09-19 22:49:06 +1200

    Add welcome message to `activate.sh`

--- activate.sh
+++ activate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This environment is useful only for running the Jekyll build for
+# the Github Pages site of this Github repo. None of this have anything
+# to do with actual TwitAnalysis code, frontend or backend.
+
 # https://blog.jez.io/ruby-virtualenvs/
 export GEM_HOME=.gems
 export GEM_PATH=
@@ -10,4 +14,7 @@ export GEM_PATH=
 # For a better `activate` scripts: (from 2012)
 # https://datagrok.org/python/activate/
 export VIRTUAL_GEM_ENV=activated
+
+echo "Activating new shell with VIRTUAL_GEM_ENV=activated and GEM_HOME=${GEM_HOME}"
+echo "Deactivate by exiting the shell, e.g. by typing 'exit' or ^D"
 exec "${@:-$SHELL}"
