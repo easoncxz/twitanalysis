56b2a81179980d11f3d5caec9606f5e3cc87044e
Eason Chen <easoncxz@gmail.com>
2020-10-10 18:49:11 +1300

    Go wild with using external React from jspm!
    This results in a 10x speedup in bundling (of course, Rollup wouldn't
    have to read React libraries from node_modules), from 7s down to 700ms.

--- frontend-app/rollup.config.js
+++ frontend-app/rollup.config.js
@@ -7,10 +7,7 @@ import path from 'path';
 import pluginNodeResolve from '@rollup/plugin-node-resolve';
 import pluginCommonjs from '@rollup/plugin-commonjs';
 import pluginNodejsGlobals from 'rollup-plugin-node-globals';
-import pluginNodejsBuiltins from 'rollup-plugin-node-builtins';
 import pluginBabel from '@rollup/plugin-babel';
-
-void pluginBabel, pluginNodejsBuiltins;
 
 function roll(input, output) {
   return {
@@ -34,23 +31,29 @@ function synonymousTsOut(input) {
 
 const reactPlaygroundBundle = {
   input: 'src/react-playground.js',
+  external: [
+    'react',
+    'react-dom',
+    'redux',
+    'redux-thunk',
+  ],
   output: {
     file: 'static/react-playground.js',
-    format: 'iife',
-    name: 'Rollup_reactPlayground',
+    format: 'es', // es, cjs, iife --- all are working
+    //name: 'Rollup_reactPlayground',
   },
   plugins: [
     // Deal with the JSX (reads `babel.config.js`)
     pluginBabel({ babelHelpers: 'bundled' }),
+
+    //// Using jspm for a minimal bundle, therefore skipping these:
     // Find React from node_modules
-    pluginNodeResolve(),
-    // React is CommonJS (i.e. without "default" member).
+    // pluginNodeResolve(),
+    // React is in CommonJS (i.e. without "default" member).
     // This adapts cjs modules to es6 modules:
-    pluginCommonjs(),
+    // pluginCommonjs(),
     // React reads process.env
-    pluginNodejsGlobals(),
-    // React uses `require` somewhere
-    // pluginNodejsBuiltins(),
+    // pluginNodejsGlobals(),
   ],
 };
 
