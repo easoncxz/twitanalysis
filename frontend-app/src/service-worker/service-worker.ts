ce38a6f927ac374d0e8d9c7a1cab4c30b3264ea1
Eason Chen <easoncxz@gmail.com>
2020-10-25 03:42:02 +1300

    Serve cached responses when offline

--- frontend-app/src/service-worker/service-worker.ts
+++ frontend-app/src/service-worker/service-worker.ts
@@ -1,17 +1,42 @@
 declare let self: ServiceWorkerGlobalScope;
 export {};
 
+const CACHE_NAME = 'v1';
+
 console.log('service-worker.js about to add listener for `install` event...');
 self.addEventListener('install', (event: ExtendableEvent) => {
   console.log('service-worker.js handling `install` event...');
-  event.waitUntil(
-    new Promise((resolve, _reject) => {
-      setTimeout(() => {
-        console.log('Install finished.');
-        resolve();
-      }, 3000);
-      console.log('Installing...');
+  return event.waitUntil(async () => {
+    const c = await caches.open(CACHE_NAME);
+    console.log('service-worker.js adding a file to cache...');
+    return c.addAll(['/main.js']);
+  });
+});
+console.log('service-worker.js added listener for `install` event.');
+
+self.addEventListener('activate', async (_event: ExtendableEvent) => {
+  const ks = await caches.keys();
+  await Promise.all(
+    ks.map((k) => {
+      if (k !== CACHE_NAME) {
+        return caches.delete(k);
+      }
     }),
   );
 });
-console.log('service-worker.js added listener for `install` event.');
+
+self.addEventListener('fetch', (event: FetchEvent) =>
+  event.respondWith(
+    (async () => {
+      const hit = await caches.match(event.request);
+      if (hit) {
+        return hit;
+      } else {
+        const resp = await fetch(event.request);
+        const c = await caches.open(CACHE_NAME);
+        await c.put(event.request, resp.clone());
+        return resp;
+      }
+    })(),
+  ),
+);
