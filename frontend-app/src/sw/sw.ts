919c3be6afc9c1a216e8b794bf642f86301b1a28
Eason Chen <easoncxz@gmail.com>
2020-10-26 02:50:34 +1300

    Adjust SW caching logic: respond-then-update

--- frontend-app/src/sw/sw.ts
+++ frontend-app/src/sw/sw.ts
@@ -3,48 +3,42 @@ export {};
 
 const CACHE_NAME = 'v1';
 
-console.log('service-worker.js about to add listener for `install` event...');
-self.addEventListener('install', (event: ExtendableEvent) => {
-  console.log('service-worker.js handling `install` event...');
-  return event.waitUntil(
+self.addEventListener('install', (event: ExtendableEvent) =>
+  event.waitUntil(
     (async () => {
       const c = await caches.open(CACHE_NAME);
-      console.log('service-worker.js adding files to cache...');
-      return c.addAll(['/main.js', '/app.html']);
+      await c.addAll(['/main.js', '/app.html', '/favicon.ico']);
+      console.log('service-worker.js added some files to cache.');
     })(),
-  );
-});
-console.log('service-worker.js added listener for `install` event.');
+  ),
+);
 
-self.addEventListener('activate', async (_event: ExtendableEvent) => {
-  // Always throw away all caches
-  const ks = await caches.keys();
-  await Promise.all(
-    ks.map((k) => {
-      //if (k !== CACHE_NAME) {
-      return caches.delete(k);
-      //}
-    }),
-  );
-});
+self.addEventListener('activate', (event: ExtendableEvent) =>
+  event.waitUntil(
+    (async () => {
+      const ks = await caches.keys();
+      const outdated = ks.filter((k) => k !== CACHE_NAME);
+      console.log(`sw.js activate: deleting old caches - ${outdated}`);
+      await Promise.all(outdated.map((k) => caches.delete(k)));
+    })(),
+  ),
+);
 
-self.addEventListener('fetch', (event: FetchEvent) => {
-  // Read-cache and update
-  // https://serviceworke.rs/strategy-cache-and-update_service-worker_doc.html
+self.addEventListener('fetch', async (event: FetchEvent) => {
   event.respondWith(
     (async () => {
       const hit = await caches.match(event.request);
       if (hit) {
+        (async () => {
+          const resp = await fetch(event.request);
+          const c = await caches.open(CACHE_NAME);
+          await c.put(event.request, resp);
+          console.log(`sw.js fetch: cache updated - ${event.request.url}`);
+        })(); // not awaiting this
+        console.log(`sw.js fetch: cache hit - ${event.request.url}`);
         return hit;
       } else {
-        event.waitUntil(
-          (async () => {
-            const resp = await fetch(event.request);
-            const c = await caches.open(CACHE_NAME);
-            await c.put(event.request, resp);
-          })(),
-        );
-        throw new Error('Cache miss');
+        return fetch(event.request);
       }
     })(),
   );
