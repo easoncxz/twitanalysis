199090a861270c972d837928e65ba309187c2b59
Eason Chen <easoncxz@gmail.com>
2020-11-04 02:10:26 +1300

    Sketch out IndexedDB schema for some Twitter things

---
+++ frontend-app/src/dom/twit-db.ts
@@ -0,0 +1,51 @@
+import { openDB } from 'idb';
+import type { IDBPDatabase, IDBPTransaction, DBSchema } from 'idb';
+
+import { User, Status } from './twitter';
+
+interface TwitDb extends DBSchema {
+  tweets: {
+    key: string;
+    value: Status;
+  };
+  users: {
+    key: string;
+    value: User;
+  };
+}
+
+export async function openMyDB(): Promise<IDBPDatabase<TwitDb>> {
+  const db: IDBPDatabase<TwitDb> = await openDB<TwitDb>('twitanalysis-idb', 1, {
+    blocked() {
+      console.log('openDB Blocked');
+    },
+    terminated() {
+      console.log("We're sad to see you go.");
+    },
+    blocking() {
+      console.log("Sorry, just a moment and we'll head off");
+    },
+    upgrade(
+      db: IDBPDatabase<TwitDb>,
+      _oldVersion: number,
+      _newVersion: number,
+      tx: IDBPTransaction<TwitDb>,
+    ) {
+      console.log('Inside openDB.upgrade');
+      db.createObjectStore('tweets', {
+        keyPath: 'id_str',
+      });
+      db.createObjectStore('users', {
+        keyPath: 'id_str',
+      });
+      return tx.done;
+    },
+  });
+  return db;
+}
+
+export async function withDB<T>(
+  action: (_: IDBPDatabase<TwitDb>) => Promise<T>,
+): Promise<T> {
+  return openMyDB().then(action);
+}
